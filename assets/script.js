ed498233ee28a460c42dab653144bf009376dfe0
Taylor Campanelli <tayascamp@yahoo.com>
2023-06-01 13:53:19 -0700

    Added function to JS file and debugged

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,24 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
-for (var x = 0; x < topics.length; x++) {
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
+function listTopics() {
+ for (var x = 0; x < topics.length; x++) {
     console.log(topics[x]);
    }
-/*
-if (topic === 'HTML') {
+}
+function selectTopic() {
+ if (randomTopic === 'HTML') {
   console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
+} else if (randomTopic === 'CSS') {
   console.log("Let's study CSS!");
-} else if (topic === 'Git') {
+} else if (randomTopic === 'Git') {
   console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
+} else if (randomTopic === 'JavaScript') {
   console.log("Let's study JavaScript!");
 } else {
   console.log('Please try again!');
 }
-*/
+}
+console.log('Here are the topics we learned through Prework:');
+listTopics();
+console.log('Which topic should we study first?');
+selectTopic();
